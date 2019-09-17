44d2879faae6b823e3500c0a8ba1145fe4310794
hcy1988 <46586403+hcy1988@users.noreply.github.com>
2019-09-17 14:18:35 +0900

    Update password.php

--- password.php
+++ password.php
@@ -1,4 +1,20 @@
 <?php
-header("content-type: text/javascript");
-echo "password = 'mypassword';\n";
+session_start();
+$_SESSION["loginname"] = $_POST["username"];
+$_SESSION["pass"] = $_POST["pass"];
+
+if($_SESSION["loginname"] != "komuro" || $_SESSION["pass"] != "pass"){
+    ?>
+    ログインに失敗しました。<br />
+    <a href="login.html">セッション生成ページ</a>
+    <?php
+    exit;
+}
+if(isset($_POST["username"])) setcookie("username", $_POST["username"], time()+120);
 ?>
+会員専用画面です。<br />
+ログイン認証に成功しました。現在ログインしている状態です。<br />
+<a href="cookie.php">マイページへ</a>
+会員専用画面です。<br />
+ログイン認証に成功しました。現在ログインしている状態です。<br />
+<a href="cookie.php">マイページへ</a>
